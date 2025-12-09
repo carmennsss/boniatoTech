fc33c58eb10c181402635ff59f2de02280925379
carmennsss <carmentamdon@gmail.com>
2025-12-09 10:09:03 +0100

    ww

--- proyecto2_DAM/src/cliente/Hola.java
+++ proyecto2_DAM/src/cliente/Hola.java
@@ -3,6 +3,8 @@ package cliente;
 public class Hola {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hola mundo desde la branch de pablo");
 		System.out.println("Patata");
 
 	}
