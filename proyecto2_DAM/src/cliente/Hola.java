f02e53ce061cedcce6ad51fa9b4cc40a0ca4a096
Pablo <pablitogonzalez0406@gmail.com>
2025-12-09 10:09:25 +0100

    push

--- proyecto2_DAM/src/cliente/Hola.java
+++ proyecto2_DAM/src/cliente/Hola.java
@@ -3,12 +3,10 @@ package cliente;
 public class Hola {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("Hola mundo desde la branch de pablo");
-=======
 		System.out.println("Patata");
 
->>>>>>> main
 	}
 
 }
