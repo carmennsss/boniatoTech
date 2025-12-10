d4a8d75948fcf185b044483df52fa5b6c586544a
Pablo <pablitogonzalez0406@gmail.com>
2025-12-10 10:47:41 +0100

    pull pablo

--- proyecto2/src/main/java/modelo/User.java
+++ proyecto2/src/main/java/modelo/User.java
@@ -7,5 +7,41 @@ public class User {
 	private String password;
 	private Rol rol;
 	
+	
+	public User(String username, String correo, String password, Rol rol) {
+		this.username = username;
+		this.correo = correo;
+		this.password = password;
+		this.rol = rol;
+	}
+	
+	
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Rol getRol() {
+		return rol;
+	}
+	public void setRol(Rol rol) {
+		this.rol = rol;
+	}
+	
+	
+	
 
 }
