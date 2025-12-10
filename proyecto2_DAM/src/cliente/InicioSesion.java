7e573fcc905bf03a6d607688cd0527c0e279b3d1
carmennsss <carmentamdon@gmail.com>
2025-12-10 08:53:16 +0100

    ASDASD

---
+++ proyecto2_DAM/src/cliente/InicioSesion.java
@@ -0,0 +1,13 @@
+package cliente;
+
+import java.io.*;
+import org.apache.commons.net.ftp.*;
+
+
+public class InicioSesion {
+	public static void main(String[] args) {
+		FTPClient cliente = new FTPClient();
+		String servFTP = "ftp.rediris.es";
+	}
+
+}
