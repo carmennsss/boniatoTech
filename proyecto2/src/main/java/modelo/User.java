bc078d218674768d1ea64a794a41e30050699d92
DanielVieitesConde <danielvieitesconde@gmail.com>
2025-12-17 09:55:38 +0100

    añadir usuario

---
+++ proyecto2/src/main/java/modelo/User.java
@@ -0,0 +1,42 @@
+package modelo;
+
+public class User {
+
+	String nombre;
+	String correo;
+	String claveCorreo;
+	String contrasena;
+	
+	public User(String nombre, String correo, String claveCorreo, String contrasena) {
+		this.nombre = nombre;
+		this.correo = correo;
+		this.claveCorreo = claveCorreo;
+		this.contrasena = contrasena;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getClaveCorreo() {
+		return claveCorreo;
+	}
+	public void setClaveCorreo(String claveCorreo) {
+		this.claveCorreo = claveCorreo;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+}
--- proyecto2/src/main/java/vista/VistaRegistroUsuarios.java
+++ proyecto2/src/main/java/vista/VistaRegistroUsuarios.java
@@ -9,84 +9,122 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
+import modelo.ModeloClienteFTP;
+import modelo.User;
 
-public class VistaRegistroUsuarios extends JFrame{
-	
+public class VistaRegistroUsuarios extends JFrame {
+
+	ModeloClienteFTP client;
 	VistaAdmin vistaAdmin;
-	
-	public VistaRegistroUsuarios(VistaAdmin vistaAdmin) {
-		this.vistaAdmin=vistaAdmin;
+	JLabel nombre;
+	JTextField textNombre;
+	JLabel correo;
+	JTextField textCorreo;
+	JLabel claveCorreo;
+	JTextField textClaveCorreo;
+	JLabel contrasena;
+	JPasswordField textContrasena;
+	JLabel confContrasena;
+	JPasswordField textConfContrasena;
+
+	public VistaRegistroUsuarios(VistaAdmin vistaAdmin, ModeloClienteFTP client) {
+		this.vistaAdmin = vistaAdmin;
+		this.client = client;
 		
 		this.setTitle("User Register");
 		this.setLayout(new BorderLayout());
-		
-		JLabel nombre = new JLabel("Name: ");
-		JTextField textNombre = new JTextField(20);
-		
-		JLabel correo = new JLabel("Address: ");
-		JTextField textCorreo = new JTextField(20);
-		
-		JLabel claveCorreo = new JLabel("Address Key: ");
-		JTextField textClaveCorreo = new JTextField(20);
-		
-		JLabel contrasena = new JLabel("Password: ");
-		JPasswordField  textContrasena = new JPasswordField (20);
-		
-		JLabel confContrasena = new JLabel("Confirm password: ");
-		JPasswordField  textConfContrasena = new JPasswordField (20);
-		
-		JButton anadir = new JButton("Register user");
+
+		nombre = new JLabel("Name: ");
+		textNombre = new JTextField(20);
+
+		correo = new JLabel("Address: ");
+		textCorreo = new JTextField(20);
+
+		claveCorreo = new JLabel("Address Key: ");
+		textClaveCorreo = new JTextField(20);
+
+		contrasena = new JLabel("Password: ");
+		textContrasena = new JPasswordField(20);
+
+		confContrasena = new JLabel("Confirm password: ");
+		textConfContrasena = new JPasswordField(20);
+
+		JButton aniadir = new JButton("Register user");
 		JButton volver = new JButton("Admin Menu");
-		
+
 		JPanel contenedor = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		JPanel layout = new JPanel(new GridLayout(5, 2, 10, 10));
 		layout.setBorder(new EmptyBorder(20, 20, 20, 20));
 
-		
 		layout.add(nombre);
 		layout.add(textNombre);
-		
+
 		layout.add(correo);
 		layout.add(textCorreo);
-		
+
 		layout.add(claveCorreo);
 		layout.add(textClaveCorreo);
-		
+
 		layout.add(contrasena);
 		layout.add(textContrasena);
-		
+
 		layout.add(confContrasena);
 		layout.add(textConfContrasena);
-		
+
 		accionBotonVolver(volver);
+		accionBotonAniadir(aniadir);
 		
 		contenedor.add(layout);
-		contenedor.add(anadir);
+		contenedor.add(aniadir);
 		contenedor.add(volver);
 
-		
 		this.add(contenedor, BorderLayout.CENTER);
 		this.setSize(500, 300);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 	public void hacerVisible() {
 		this.setVisible(true);
 	}
-	
+
 	public void accionBotonVolver(JButton boton) {
 		boton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	
-            	VistaRegistroUsuarios.this.setVisible(false);
-            	vistaAdmin.hacerVisible();
-            }
-        });
+			public void actionPerformed(ActionEvent e) {
+
+				VistaRegistroUsuarios.this.setVisible(false);
+				vistaAdmin.hacerVisible();
+			}
+		});
 	}
-	
+
+	public void accionBotonAniadir(JButton boton) {
+
+		boton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				User usuario;
+				char[] contrasena = textContrasena.getPassword();
+	            char[] confContrasena = textConfContrasena.getPassword();
+
+	            if (!java.util.Arrays.equals(contrasena, confContrasena)) {
+					JOptionPane.showMessageDialog(VistaRegistroUsuarios.this, "Password doesn't match", "Error",
+							JOptionPane.ERROR_MESSAGE);
+				} else {
+					usuario = new User(textNombre.getText(), textCorreo.getText(), textClaveCorreo.getText(),
+							textContrasena.getPassword().toString());
+					client.aniadirUsuario(usuario.getNombre(),usuario.getContrasena());
+					JOptionPane.showMessageDialog(VistaRegistroUsuarios.this, "User registered correctly", "",
+							JOptionPane.INFORMATION_MESSAGE);
+				}
+
+			}
+		});
+
+	}
+
 }
