40a8aadc627424cd5b5304ef64a6d2e6f665973e
Pablo <pablitogonzalez0406@gmail.com>
2025-12-18 09:54:31 +0100

    commit

--- proyecto2/src/main/java/controladorCorreos/ControladorCorreos.java
+++ proyecto2/src/main/java/controladorCorreos/ControladorCorreos.java
@@ -94,18 +94,16 @@ public class ControladorCorreos {
 
 	// ELIMINAR
 	public void eliminarCorreoSeleccionado(Correo correo) {
-		new Thread(() -> {
-	        try {
-	            gestion.eliminarCorreoIMAP(HOSTIMAP, CORREO, PASSWORD_APLICACION, correo.getMessageId());
+		try {
 
-	            SwingUtilities.invokeLater(() -> {
-	                correos.remove(correo);
-	                vistaGeneral.cargarCorreos(new ArrayList<>(correos)); 
-	            });
-	        } catch (Exception e) {
-	            e.printStackTrace();
-	        }
-	    }).start();
+			gestion.eliminarCorreoPOP3(HOST, "recent:" + CORREO, PASSWORD_APLICACION, correo);
+
+	        correos.remove(correo);
+	        vistaGeneral.cargarCorreos(correos);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	// LEIDO
