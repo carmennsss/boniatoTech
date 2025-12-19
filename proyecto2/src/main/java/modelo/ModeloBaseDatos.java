5e4661d1ddc113ac55ab1cb0d8ad0f4d79924fe5
Pablo <pablitogonzalez0406@gmail.com>
2025-12-19 08:23:05 +0100

    Update ModeloBaseDatos.java

--- proyecto2/src/main/java/modelo/ModeloBaseDatos.java
+++ proyecto2/src/main/java/modelo/ModeloBaseDatos.java
@@ -291,8 +291,8 @@ public class ModeloBaseDatos {
     }
 
     public boolean eliminarUsuario(String email) {
-        String sql = "DELETE FROM usuarios WHERE email = ?";
-        try (PreparedStatement ps = conexion.prepareStatement(sql)) {
+        String sql = "DELETE FROM usuarios WHERE email = ?s";
+        try (PreparedStatement ps = getConexion().prepareStatement(sql)) {
             ps.setString(1, email);
             return ps.executeUpdate() > 0;
         } catch (SQLException e) {
